afbd820083f9e928796e2810e5d3ac8d2c98b685
Nikhil_Sai_Nambula <nambula.nikhilsai@gmail.com>
2025-02-05 22:22:16 +0530

    added cors

--- src/main/java/com/example/ecommerce/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -3,7 +3,7 @@ package com.example.ecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +11,7 @@ import com.example.ecommerce.domain.Product;
 import com.example.ecommerce.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
 	
 	@Autowired
